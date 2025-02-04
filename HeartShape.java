4c6629566ef8a393555c0bc179f149f8a545ccf5
heetchauhan02 <heetchauhan02@gmail.com>
2025-02-04 16:45:55 +0530

    Files

---
+++ HeartShape.java
@@ -0,0 +1,19 @@
+public class HeartShape {
+    public static void main(String[] args) {
+        String[] heart = {
+            "  **     **  ",
+            " ****   **** ",
+            "****** ******",
+            " *********** ",
+            "  *********  ",
+            "   *******   ",
+            "    *****    ",
+            "     ***     ",
+            "      *      "
+        };
+
+        for (String line : heart) {
+            System.out.println(line);
+        }
+    }
+}
