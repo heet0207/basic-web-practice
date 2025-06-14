e2a03e55bde963d8c5cf65fafa91a54c10c5c3d4
heetchauhan02 <heetchauhan02@gmail.com>
2025-06-14 20:03:58 +0530

    New Files

---
+++ CLL.java
@@ -0,0 +1,15 @@
+import java.util.*;
+@SuppressWarnings("All")
+class CLL{
+    public static void main(String[] args) {
+        
+    }
+}
+
+class neel{
+
+    class Node{
+        int data;
+        
+    }
+}
