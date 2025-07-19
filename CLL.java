ebefe1506a304f04a2af4a7b1bac667102561518
heetchauhan02 <heetchauhan02@gmail.com>
2025-07-19 05:34:24 +0530

    Files

--- CLL.java
+++ CLL.java
@@ -2,7 +2,9 @@ import java.util.*;
 @SuppressWarnings("All")
 class CLL{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        neel n = new neel();
+        boolean  b = true;
     }
 }
 
@@ -10,6 +12,28 @@ class neel{
 
     class Node{
         int data;
-        
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+
+    Node Head = null;
+
+    void insertFirst(int d){
+        Node n1 = new Node(d);
+        if(Head == null){
+            Head = n1;
+            n1.next = Head; // Point to itself
+        } else {
+            Node temp = Head;
+            while(temp.next != Head) {
+                temp = temp.next; // Traverse to the last node
+            }
+            temp.next = n1; // Link the last node to the new node
+            n1.next = Head; // New node points to the head
+            Head = n1; // Update head to new node
+        }
     }
 }
---
+++ Que2.java
@@ -0,0 +1,7 @@
+import java.io.*;
+class Que2{
+    public static void main(String[] args) throws IOException {
+    FileWriter fw = new FileWriter("output.txt");
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
