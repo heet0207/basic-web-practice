bdeeb2721d2c9d126cf7e356ea7dc5060d6f4fd1
heet <heetchauhan02@gmail.com>
2025-12-08 03:23:02 +0530

    Files

--- call.py
+++ call.py
@@ -29,3 +29,32 @@ else:
     print(k)
 finally:
     print("Execution completed.")
+
+age==-1 # type: ignore
+if age<0: # type: ignore
+    raise Exception('age')
+
+class B(Exception):
+    pass
+l=5
+if l<10:
+    raise B('l is less than 10')
+
+
+def f(a):
+    if type(a)!=str:
+        raise TypeError('a is not str')
+    if (not a):
+        raise ValueError('a is empty string')
+    a.strip()
+    if a=='':
+        return True
+    return False
+
+try:
+    a=123
+    print('is Empty',f(a))
+except TypeError as e:
+    print('T Error ',e)
+except ValueError as e:
+    print("V Error ", e)
