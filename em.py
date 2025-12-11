1503819a52d955f77779e8bdbbc77ba808ff6e72
heet <heetchauhan02@gmail.com>
2025-12-11 10:44:27 +0530

    Files

--- em.py
+++ em.py
@@ -58,3 +58,62 @@ if annual_pay > 250000:
 print("Gross Pay of an Employee is:", GP)
 print("Annual income of an Employee is:", annual_pay)
 print("Income Tax to be paid by an Employee is:", tax)
+
+
+
+
+#534
+class pizza:
+    def __init__(self,size,toppings,cheese):
+        self.size=size
+        self.toppings=toppings
+        self.cheese=cheese
+    def price(self):
+        self.cost=0
+        if self.size=='small':
+            self.cost+=50
+        elif self.size=='medium':
+            self.cost+=100
+        elif self.size=='large':
+            self.cost+=200
+        else:
+            print('Enter valid size')
+        topping_price_20=['corn','tomato','onion','capsicum']
+        topping_price_50=['mushroom','olives','broccoli']
+        for topping in self.toppings:
+            if topping in topping_price_20:
+                self.cost+=20
+            else:
+                self.cost+=50
+        self.cost+=50*(len(self.cheese))
+        return self.cost
+class Order:
+    def __init__(self,name,customerid):
+        self.name=name
+        self.customerid=customerid
+    def order(self,n):
+        self.pizzas=[]
+        for i in range(n):
+            toppings=[]
+            cheese=[]
+            print('Customize Pizza',i+1)
+            size=(input('Enter the size of pizza :'))
+            t=int(input('Enter No. of topping :'))
+            for i in range(t):
+                toppings.append(input('Enter topping :'))
+            c=int(input('Enter no of cheese :'))
+            for i in range(c):
+                cheese.append(input('Enter cheese names :'))
+            self.pizzas.append(pizza(size,toppings,cheese))
+    def bill(self):
+        self.total=0
+        count=1
+        for p in self.pizzas:
+            print('Pizza',count)
+            print(p.size,p.toppings,p.cheese)
+            self.total+=p.price()
+            count+=1
+        print('Total bill amount:',self.total)
+order1=Order('As',1)
+order1.order(2)
+order1.bill()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,42 +1,54 @@
-class Store:
-    def __init__(self):
-        self.items = {}
-
-    def add_item(self, code, price):
-        self.items[code] = price
-
-    def display_items(self):
-        print("\nItem Code\tPrice")
-        for code, price in self.items.items():
-            print(f"{code}\t\t{price}")
-
-    def generate_bill(self):
-        quantities = {}
-        print("\nEnter quantity of each item:")
-        for code in self.items:
-            qty = int(input(f"Enter quantity of {code} : "))
-            quantities[code] = qty
-
-        print("\n***********************Bill************************")
-        print("ITEM\tPRICE\tQUANTITY\tSUBTOTAL")
-        total = 0
-        for code, price in self.items.items():
-            subtotal = price * quantities[code]
-            total += subtotal
-            print(f"{code}\t{price}\t{quantities[code]}\t\t{subtotal}")
-
-        print("***************************************************")
-        print("Total= ", total)
-
-
-
-store = Store()
-n = int(input("Enter no of items: "))
-
-for i in range(n):
-    code = input("Enter code of item: ")
-    price = int(input("Enter cost of item: "))
-    store.add_item(code, price)
-
-store.display_items()
-store.generate_bill()
+class pizza:
+    def __init__(self,size,toppings,cheese):
+        self.size=size
+        self.toppings=toppings
+        self.cheese=cheese
+    def price(self):
+        self.cost=0
+        if self.size=='small':
+            self.cost+=50
+        elif self.size=='medium':
+            self.cost+=100
+        elif self.size=='Large':
+            self.cost+=200
+        else:
+            print('Enter valid size')
+        topping_price_20=['corn','tomato','onion','capsicum']
+        topping_price_50=['mushroom','olives','broccoli']
+        for topping in self.toppings:
+            if topping in topping_price_20:
+                self.cost+=20
+            else:
+                self.cost+=50
+        self.cost+=50*(len(self.cheese))
+        return self.cost
+class Order:
+    def __init__(self,name,customerid):
+        self.name=name
+        self.customerid=customerid
+    def order(self,n):
+        self.pizzas=[]
+        for i in range(n):
+            toppings=[]
+            cheese=[]
+            print('Customize Pizza',i+1)
+            size=(input('Enter the size of pizza'))
+            t=int(input('Enter No. of topping'))
+            for i in range(t):
+                toppings.append(input('Enter topping:'))
+            c=int(input('Enter no of cheese:'))
+            for i in range(c):
+                cheese.append(input('Enter cheese names'))
+            self.pizzas.append(pizza(size,toppings,cheese))
+    def bill(self):
+        self.total=0
+        count=1
+        for p in self.pizzas:
+            print('Pizza',count)
+            print(p.size,p.toppings,p.cheese)
+            self.total+=p.price()
+            count+=1
+        print('Total bill amount:',self.total)
+order1=Order('As',1)
+order1.order(2)
+order1.bill()
