5efa58bb489ba7b92f478137e371579c6170b41c
heet <heetchauhan02@gmail.com>
2025-12-26 11:46:49 +0530

    Files

---
+++ T4/poly.py
@@ -0,0 +1,14 @@
+# Polymorphism
+
+n1=1
+n2=2
+print(n1+n2)
+
+n1='Python'
+n2='Programming'
+print(n1+n2)
+
+#function Polymorphism
+print(len('Programming'))
+print(len(['Python','Java','C']))
+print(len({'Name':'As','Age':23}))
---
+++ T4/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(len('Programming'))
+print(len(['Python','Java','C']))
+print(len({'Name':'As','Age':23}))
