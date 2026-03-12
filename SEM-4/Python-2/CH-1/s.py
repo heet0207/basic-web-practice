fdb9d68c589dd463f6ad09873cad80ccbee68c9e
heet <heetchauhan02@gmail.com>
2026-03-12 09:02:00 +0530

    Files

---
+++ SEM-4/Python-2/CH-1/s.py
@@ -0,0 +1,10 @@
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+
+data = {
+    'Name':['John', 'Alice', 'Bob', 'Charlie'],
+    'Age':[25, 30, 22, 28],
+    'City':['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+df=pd.DataFrame(data)
+df
