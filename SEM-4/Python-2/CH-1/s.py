b42f1c83c53a19df9c09558a9ae745365d90859c
heet <heetchauhan02@gmail.com>
2026-03-12 09:26:30 +0530

    `Added multiple examples of creating a Pandas DataFrame`

--- SEM-4/Python-2/CH-1/s.py
+++ SEM-4/Python-2/CH-1/s.py
@@ -1,3 +1,6 @@
+# The code you provided demonstrates different ways to create a DataFrame in Python using the Pandas library. Here's a breakdown of each section:
+
+# DataFrame creation using dictionary
 import pandas as pd # type: ignore
 import numpy as np # type: ignore
 
@@ -7,4 +10,55 @@ data = {
     'City':['New York', 'Los Angeles', 'Chicago', 'Houston']
 }
 df=pd.DataFrame(data)
-df
+print(df)
+print()
+
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+
+data = {
+    'Name':['John', 'Alice', 'Bob', 'Charlie'],
+    'Age':[25, 30, 22, 28],
+    'City':['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+df=pd.DataFrame(data,index=['A', 'B', 'C', 'D'])
+print(df)
+print()
+
+# DataFrame creation using list of lists
+
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+
+data = [
+    ['John', 25, 'New York'],
+    ['Alice', 30, 'Los Angeles'],
+    ['Bob', 22, 'Chicago'],
+]
+df=pd.DataFrame(data, columns=['Name', 'Age', 'City'])
+print(df)
+print()
+
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+
+data = [
+    ['John', 25, 'New York'],
+    ['Alice', 30, 'Los Angeles'],
+    ['Bob', 22, 'Chicago'],
+]
+df=pd.DataFrame(data, columns=['Name', 'Age', 'City'],index=['A', 'B', 'C'])
+print(df)
+print()
+
+# Dataframe creation using list of dictionaries
+
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+data = [
+    {'Name':'John', 'Age':25, 'City':'New York'},
+    {'Name':'Alice', 'Age':30, 'City':'Los Angeles'},
+    {'Name':'Bob', 'Age':22, 'City':'Chicago'},
+]
+df=pd.DataFrame(data)
+print(df)
