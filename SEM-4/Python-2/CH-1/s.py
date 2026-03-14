a2ef4c589376940c341c2be39d8c9cb798474335
heet <heetchauhan02@gmail.com>
2026-03-14 10:03:50 +0530

    `Updated Python code for SEM-4/Python-2/CH-1 with new data and modifications to existing code`

--- SEM-4/Python-2/CH-1/s.py
+++ SEM-4/Python-2/CH-1/s.py
@@ -168,7 +168,23 @@ data = {
         'C':[np.nan,13,14,15],
         'D':[20,np.nan,33,45]
 }
-
 df=pd.DataFrame(data)
 df.set_index('A',drop=True,inplace=True)
+df.reset_index(inplace=True)
+# df.set_index('B',drop=True,inplace=True)
+df['E']=['Surat','Kheda','Rajkot','Ahmdabad']
+df.loc[4]=[12,32,25,25,'Ahmdabad']
 print(df)
+
+
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+
+df=pd.read_csv('titanic.csv')
+# print(df)
+# print(df.describe(include='int64'))
+# print(df.describe(exclude='int64'))
+#print(df.describe(include='all'))
+#print(df['embarked'].unique())
+#print(df['age'].unique())
+print(df['sex'].unique())
--- SEM-4/Python-2/CH-1/tempCodeRunnerFile.py
+++ SEM-4/Python-2/CH-1/tempCodeRunnerFile.py
@@ -1,12 +1,11 @@
 import pandas as pd # type: ignore
 import numpy as np # type: ignore
-data = {
-        'A':[1,2,4,7],
-        'B':[8,9,np.nan,10],
-        'C':[np.nan,13,14,15],
-        'D':[20,np.nan,33,45]
-}
 
-df=pd.DataFrame(data)
-df.set_index('A',drop=True,inplace=True)
-print(df)
+df=pd.read_csv('titanic.csv')
+# print(df)
+# print(df.describe(include='int64'))
+# print(df.describe(exclude='int64'))
+#print(df.describe(include='all'))
+#print(df['embarked'].unique())
+#print(df['age'].unique())
+print(df['sex'].unique())
