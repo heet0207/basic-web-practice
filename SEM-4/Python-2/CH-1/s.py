04dd5a26836b9d9d480fee2537c2cc120989549a
heet <heetchauhan02@gmail.com>
2026-03-14 10:42:39 +0530

    Files

--- SEM-4/Python-2/CH-1/s.py
+++ SEM-4/Python-2/CH-1/s.py
@@ -181,6 +181,10 @@ import pandas as pd # type: ignore
 import numpy as np # type: ignore
 
 df=pd.read_csv('titanic.csv')
+df['age']=df['age'].fillna(int(df['age'].mean()))
+print(df['age'])
+# print(df['age'].fillna(0))
+# print(df.isna().sum())
 # print(df)
 # print(df.describe(include='int64'))
 # print(df.describe(exclude='int64'))
@@ -193,5 +197,5 @@ df=pd.read_csv('titanic.csv')
 # print(df['sex'].value_counts()['male'])
 # print((df['sex']=='male').sum())
 # print(df.nunique())
-print(df.isnull())
-print(df.isnull().sum())
+# print(df.isnull())
+# print(df.isnull().sum())
--- SEM-4/Python-2/CH-1/tempCodeRunnerFile.py
+++ SEM-4/Python-2/CH-1/tempCodeRunnerFile.py
@@ -1,18 +1,7 @@
+
 import pandas as pd # type: ignore
 import numpy as np # type: ignore
 
 df=pd.read_csv('titanic.csv')
-# print(df)
-# print(df.describe(include='int64'))
-# print(df.describe(exclude='int64'))
-#print(df.describe(include='all'))
-#print(df['embarked'].unique())
-# print(df['age'].nunique())
-# print(df['sex'].unique())
-# print(df['embarked'].value_counts()['S'])
-# print((df['embarked']=='S').sum())
-# print(df['sex'].value_counts()['male'])
-# print((df['sex']=='male').sum())
-# print(df.nunique())
-print(df.isnull())
-print(df.isnull().sum())
+df['age']=df['age'].fillna(int(df['age'].mean()))
+print(df['age'])
