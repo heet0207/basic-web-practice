22824104d31334c22dade5c8b30357a5222f2289
heet <heetchauhan02@gmail.com>
2026-03-18 10:24:59 +0530

    Refactor shape perimeter calculations and improve file reading logic

--- SEM-4/FSD-2/CH-2/F6.js
+++ SEM-4/FSD-2/CH-2/F6.js
@@ -1,15 +1,33 @@
+// const Shape = [
+//         {'Name':'Circle','Diameter':8},
+//         {'Name':'Square','Side':10}]
+// let fs = require('fs');
+// let jd = JSON.stringify(Shape);
+// fs.writeFileSync('Shape.txt', jd);
+// fs.readFileSync('Shape.txt','utf-8')
+//     let x = Shape[0].Diameter;
+//     let y = Shape[1].Side;
+//     let circle = 2*3.14*(x/2);
+//     let square = 4*y;
+//     console.log('Perimeter of Circle: '+circle);
+//     console.log('Perimeter of Square: '+square);
+// fs.appendFileSync('Shape.txt', '\nPerimeter of Circle: '+circle);
+// fs.appendFileSync('Shape.txt', '\nPerimeter of Square: '+square);
+
+
 const Shape = [
         {'Name':'Circle','Diameter':8},
         {'Name':'Square','Side':10}]
 let fs = require('fs');
 let jd = JSON.stringify(Shape);
 fs.writeFileSync('Shape.txt', jd);
-fs.readFileSync('Shape.txt','utf-8')
-    let x = Shape[0].Diameter;
-    let y = Shape[1].Side;
+let r = fs.readFileSync('Shape.txt','utf-8')
+    let b = JSON.parse(r);
+    let x = b[0].Diameter;
+    let y = b[1].Side;
     let circle = 2*3.14*(x/2);
     let square = 4*y;
     console.log('Perimeter of Circle: '+circle);
     console.log('Perimeter of Square: '+square);
-fs.appendFileSync('Shape.txt', '\nPerimeter of Circle: '+circle);
-fs.appendFileSync('Shape.txt', '\nPerimeter of Square: '+square);
+fs.appendFileSync('Shape.txt', '\nPerimeter of Circle: '+circle + '\nPerimeter of Square: '+square);
+
