8845cfe4d0a2221212badd1c46f4184248366c1f
heet <heetchauhan02@gmail.com>
2026-03-24 13:13:53 +0530

    Add URL parsing and file writing functionality in p3.js

---
+++ SEM-4/FSD-2/CH-2/p3.js
@@ -0,0 +1,15 @@
+let fs = require('fs')
+let v = require('url')
+var addr = 'http://localhost:8080/default.html ?year=2025&month=Feb'
+let x = v.parse(addr,true)
+// console.log(x)
+fs.writeFileSync('z.txt',JSON.stringify(x))
+console.log(fs.readFileSync('z.txt','utf-8'))
+// console.log(x.query.year)
+// console.log(x.query.month)
+// if(x.query.year % 4 ==0){
+//     console.log('Leap')
+// }
+// else{
+//     console.log('Not Leap')
+// }
