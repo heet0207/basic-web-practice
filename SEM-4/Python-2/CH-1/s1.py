99f71a4ade0abdf6425c232909fc72b338df0ccb
heet <heetchauhan02@gmail.com>
2026-03-24 11:38:13 +0530

    Refactor visualizations in s1.py and tempCodeRunnerFile.py for Titanic dataset analysis

--- SEM-4/Python-2/CH-1/s1.py
+++ SEM-4/Python-2/CH-1/s1.py
@@ -4,5 +4,21 @@ import pandas as pd # type: ignore
 
 df = pd.read_csv('titanic.csv') # type: ignore
 corr = df.corr(numeric_only=True)
-sns.heatmap(corr, annot=True,cmap='coolwarm',cbar=True)
+sns.heatmap(corr, annot=True,cbar=True)
+plt.show()
+
+import seaborn as sns # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+import pandas as pd # type: ignore
+
+df = pd.read_csv('titanic.csv') # type: ignore
+sns.scatterplot(x='age',y='fare',data=df, hue='survived')
+plt.show()
+
+import seaborn as sns # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+import pandas as pd # type: ignore
+
+df = pd.read_csv('titanic.csv') # type: ignore
+sns.boxplot(x='pclass',y='fare',data=df, hue='survived')
 plt.show()
--- SEM-4/Python-2/CH-1/tempCodeRunnerFile.py
+++ SEM-4/Python-2/CH-1/tempCodeRunnerFile.py
@@ -1,22 +1,8 @@
-import pandas as pd # type: ignore
-import numpy as np # type: ignore
 
-data={
-    "Deptartment":['IT','HR','IT','HR','SALES','IT'],
-    "Salary":[5000,4000,4500,6000,7500,8000],
-    "Age":[25,30,28,32,40,1000],
-}
-df=pd.DataFrame(data)
-Q1 = df['Age'].quantile(0.25)
-Q2 = df['Age'].quantile(0.5)
-Q3 = df['Age'].quantile(0.75)
-IQR = Q3 - Q1
-lower = Q1 - 1.5 * IQR
-upper = Q3 + 1.5 * IQR
-print(lower)
-print(upper)
-Outliers = df[(df['Age'] < lower) | (df['Age'] > upper)]
-print(Outliers)
-print(IQR)
-clean = df[(df['Age'] >= lower) & (df['Age'] <= upper)]
-print(clean)
+import seaborn as sns # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+import pandas as pd # type: ignore
+
+df = pd.read_csv('titanic.csv') # type: ignore
+sns.boxplot(x='pclass',y='fare',data=df, hue='survived')
+plt.show()
